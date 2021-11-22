448ffaad4bd89bbc1f298c65aa1d702a91f19845
Sylvain Téchené <sylvain.techene@wanadoo.fr>
2021-11-22 12:51:17 +0100

    #10 bugfix/ lotery shuffle
    Co-authored-by: VirgilBontempi <VirgilBontempi@users.noreply.github.com>

--- src/Lotery.ts
+++ src/Lotery.ts
@@ -4,20 +4,23 @@ export class Lotery {
     private participants: string[];
 
     constructor(participants: string[]) {
-
-        this.participants = _.shuffle(participants);
-
+        do {
+            this.participants = _.shuffle(participants);
+        } while (this.arraysAreEquals(this.participants, participants))
     }
 
     getAssociatedParticipants(): ParticipantAssociation[] {
         const receiverList: string[] = [...this.participants];
         receiverList.push(receiverList.shift() ?? '');
 
-
         return this.participants.map((participant, index) => ({
             giver: participant,
             receiver: receiverList[index]
         }));
+    }
+
+    arraysAreEquals(array1: string[], array2: string[]): boolean {
+        return array1.toString() === array2.toString();
     }
 }
 
